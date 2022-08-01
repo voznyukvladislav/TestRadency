bf1aaac3fdda1a27ef1136cb82e3bb03d83dee3d
Владислав Вознюк <vladius990@gmail.com>
2022-08-01 19:02:09 +0300

    Result comments

--- TestRadency/Program.cs
+++ TestRadency/Program.cs
@@ -14,8 +14,8 @@ string Order(string input)
     return Element.ToString(elements);
 }
 
-Console.WriteLine(Order("45 34 24 108 76 58 64 130 80"));
-Console.WriteLine(Order("    2022 70 123    3344 13 "));
-Console.WriteLine(Order("       31  13     22     17  0    8  "));
-Console.WriteLine(Order("      25      52   34      43      80   71    90   81"));
-Console.WriteLine(Order("      asf8     13a     14     8    41  13    9 "));
+Console.WriteLine(Order("45 34 24 108 76 58 64 130 80")); // 130 24 34 80 108 45 64 58 76
+Console.WriteLine(Order("    2022 70 123    3344 13 ")); // 13 123 2022 70 3344
+Console.WriteLine(Order("       31  13     22     17  0    8  ")); // 0 13 22 31 17 8
+Console.WriteLine(Order("      25      52   34      43      80   71    90   81")); // 25 34 43 52 71 80 81 90
+Console.WriteLine(Order("      asf8     13a     14     8    41  13    9 ")); // 13 14 41 8 9 
